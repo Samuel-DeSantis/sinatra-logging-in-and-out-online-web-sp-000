c781255304064e76f87a16014ff5383e2b154e3f
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-04-29 21:33:37 -0600

    commit tree 9492019d71a3f0618cff349f54d969b98367f5d5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username]) #=> Finding user by username
+    if @user != nil && @user == params[:password] #=> Making sure there is a user
+      session[:user_id] = @user.id                #   and that the passwords match
+      redirect '/account'                         #   if correct, route to account
+    end
+    redirect '/error' #=> if passwords don't match, then path to error
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
